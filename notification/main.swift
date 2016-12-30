3e6b46991c23228bdc2b492278f40c56ca83cdc6
Kilian Koeltzsch <me@kilian.io>
2016-12-30 13:45:54 +0100

    Initial Commit

---
+++ notification/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  notification
+//
+//  Created by Kilian Költzsch on 30/12/2016.
+//  Copyright © 2016 Kilian Koeltzsch. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
