9ca549985d5531896a2aa958678a2a3ae8b067fa
Kilian Koeltzsch <me@kilian.io>
2016-12-30 14:16:59 +0100

    implement basic idea/functionality

--- notification/main.swift
+++ notification/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
+class Notification {
+    var name: NSNotification.Name?
 
+    init(notificationName: String? = nil) {
+        if let notificationName = notificationName {
+            name = NSNotification.Name(notificationName)
+        } else {
+            name = nil
+        }
+        DistributedNotificationCenter.default().addObserver(self, selector: #selector(receive), name: name, object: nil)
+    }
+
+    deinit {
+        DistributedNotificationCenter.default().removeObserver(self, name: name, object: nil)
+    }
+
+    @objc func receive(notification: NSNotification) {
+        print(notification)
+    }
+}
+
+let n = CommandLine.arguments.count > 1 ? Notification(notificationName: CommandLine.arguments[1]) : Notification()
+RunLoop.main.run(until: Date.distantFuture)
